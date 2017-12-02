3cfa259b60b467245c9db2fc94a8a9bbce987348
Rimas G <rgaina@lighthousenetwork.com>
2017-12-02 17:00:11 +0200

    Added ProximitySensor handler

--- app/src/main/java/com/demolabs/push_upscounter/MainActivity.kt
+++ app/src/main/java/com/demolabs/push_upscounter/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.demolabs.push_upscounter
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import com.demolabs.push_upscounter.handlers.SensorHandler
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private var sensorEventVal: Float = 0f
+    private lateinit var sensorHandler: SensorHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        sensorHandler = SensorHandler(this)
         initControls()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorHandler.onResume(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorHandler.onPause(this)
     }
 
     private fun initControls() {
@@ -20,5 +37,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun toggleSensor() {
         Toast.makeText(this, "Sensor enabled / disabled", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, i: Int) {
+        // no-op
+    }
+
+    override fun onSensorChanged(sensorEvent: SensorEvent?) {
+        sensorEventVal = sensorEvent?.values?.get(0) ?: 0f
+        proximityValue.text = sensorEventVal.toString()
     }
 }
